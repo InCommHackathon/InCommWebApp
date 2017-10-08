a9a1c5fe3141f9deda80763c2fe1414e9bbe39ec
mylesandre1124@gmail.com <megamacman11>
2017-10-07 22:15:54 -0400

    commit

--- src/Servlet/Servlet.java
+++ src/Servlet/Servlet.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 public class Servlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("Hello");
+        response.sendRedirect("https://www.gmail.com");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
